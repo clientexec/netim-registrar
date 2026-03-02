19c9f7391679c38f43dc138139ae29f29a8e0e6e
Matt <matt@clientexec.com>
2026-03-02 19:29:45 +0000

    Updates for version v7.0.0

--- api/NetimAPIException.php
+++ api/NetimAPIException.php
@@ -3,14 +3,17 @@
 namespace Netim
 {
     use Exception;
+
     // Generic class for a client API Exception. Used to standardize API Exceptions so the CMS code does not depend of the underlying architecture (SOAP, REST ...)
-	class NetimAPIException extends Exception
-	{
-        public function __construct($message, $code = 0, Exception $previous = null) {
+    class NetimAPIException extends Exception
+    {
+        public function __construct($message, $code = 0, ?Exception $previous = null)
+        {
             parent::__construct($message, $code, $previous);
         }
 
-        public function __toString() {
+        public function __toString()
+        {
             return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
         }
     }
